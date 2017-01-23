62115610307e20a398849bea87051acb5f1560d7
EddieYiLiZhang <eddieyili.zhang@benilde.edu.ph>
2017-01-23 21:12:35 +0800

    Updated version 1-23-2017

--- sis/sis/register.php
+++ sis/sis/register.php
@@ -1,85 +1,88 @@
 <?php
-		#write your comment here
-		//write your comment here
-		/*write your comment here*/
-		$studentNo = "";
-		$lastName = "";
-		$firstName = "";
-		$emailAddress = "";
-		$password = "";
-		$birthDate = "null";
+    #write your comment here
+    //write your comment here
+    /*write your comment here*/
+    $studentNo = "";
+    $lastName = "";
+    $firstName = "";
+    $emailAddress = "";
+    $password = "";
+    $birthDate = "null";
 ?>
+
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Student Registration</title>
-		<link href="css/bootstrap.min.css" rel="stylesheet" />
-	</head>
-	<body>
-		<div class="container">
-			<h1 class="text-center">Student Registration </h1>
-			<div class="col-lg-offset-3 col-lg-6">
-				<div class="well">
-					<form method="POST" action="welcome.php" class="form-horizontal">
-							<div class="form-group">
-								<label class="control-label col-lg-4">
-									Student ID
-								</label>
-								<div class="col-lg-8">
-									<input type="text" name="sid" class="form-control" required/>
-								</div>
-							</div>
-							<div class="form-group">
-								<label class="control-label col-lg-4">
-									LastName
-								</label>
-								<div class="col-lg-8">
-									<input type="text" name="ln" class="form-control" required/>
-								</div>
-							</div>
-							<div class="form-group">
-								<label class="control-label col-lg-4">
-									FirstName
-								</label>
-								<div class="col-lg-8">
-									<input type="text" name="fn" class="form-control" required/>
-								</div>
-							</div>
-							<div class="form-group">
-								<label class="control-label col-lg-4">
-									Email Address
-								</label>
-								<div class="col-lg-8">
-									<input type="email" name="email" class="form-control" required/>
-								</div>
-							</div>
-							<div class="form-group">
-								<label class="control-label col-lg-4">
-									Password
-								</label>
-								<div class="col-lg-8">
-									<input type="password" name="pwd" class="form-control" required/>
-								</div>
-							</div>
-							<div class="form-group">
-								<label class="control-label col-lg-4">
-									Birthdate
-								</label>
-								<div class="col-lg-8">
-									<input type="date" name="bday" class="form-control" required/>
-								</div>
-							</div>
-							<div class="form-group">
-								<div class="col-lg-offset-4 col">
-									<button name="register" class="btn btn-success">
-										Register
-									</button>
-							</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</body>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Register Form</title>
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="css/style.css">
 
+  
+</head>
 
+<body>
+  <div class="form">     
+      <div class="tab-content">
+        <div id="signup">   
+          <h1>Register</h1>
+          
+          <form method="POST" action="welcome.php">
+          
+          <div class="field-wrap">
+            <label>
+              Student ID<span class="req">*</span>
+            </label>
+            <input type="number" required name="sid" autocomplete="off"/>
+          </div>
+
+          <div class="top-row">
+            <div class="field-wrap">
+              <label>
+                Last Name<span class="req">*</span>
+              </label>
+              <input type="text" required name="ln" autocomplete="off" />
+            </div>
+
+        
+            <div class="field-wrap">
+              <label>
+                First Name<span class="req">*</span>
+              </label>
+              <input type="text" required name="fn" autocomplete="off"/>
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email" required name="email" autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Password<span class="req">*</span>
+            </label>
+            <input type="password" required name="pwd" autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+          <label>Birthdate<span class="req">*</span></label><br/><br/>
+            <label>
+              <span class="req"></span>
+            </label>
+            <input type="date" name="bday" required name="bday" autocomplete="off"/>
+          </div>
+
+          <button type="submit" class="button button-block" name="register" class="btn btn-success"/>Register</button>
+
+          </form>
+        </div>
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/index.js"></script>
+
+</body>
 </html>
--- welcome.php
+++ welcome.php
@@ -1,27 +1,60 @@
-<?php
-	if (isset($_POST['register']))
-	{
-		$studentNo = $_POST['sid'];
-		$lastName = $_POST['ln'];
-		$firstName = $_POST['fn'];
-		$emailAddress = $_POST['email'];
-		$password = $_POST['pwd'];
-		$birthDate = $_POST['bday'];
-
-		echo "WELCOME, " . $lastName . " " . $firstName . "!</br>" . 
-		"Your ID number is " . $studentNo . "!</br>" . 
-		"Your Email Address is " . $emailAddress . "! </br>" .
-		"Your Password is ". $password . "! </br>" .
-		"Your Birthday is ". $birthDate . "! </br>" . 
-		"The Time is " . date('c');
-	}
-	else
-	{
-		header('location: register.php');
-	}
-
-?>
-
-
 <!DOCTYPE html>
-<html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Register Form</title>
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+  <div class="form">     
+      <div class="tab-content">
+        <div id="signup">   
+          <h1>WELCOME</h1>
+          
+          <form method="POST" action="register.php">
+          
+          <div class="field-wrap">
+			<label>
+			<?php
+			if (isset($_POST['register']))
+			{
+				$studentNo = $_POST['sid'];
+				$lastName = $_POST['ln'];
+				$firstName = $_POST['fn'];
+				$emailAddress = $_POST['email'];
+				$password = $_POST['pwd'];
+				$birthDate = $_POST['bday'];
+
+				echo "WELCOME, " . $lastName . " " . $firstName . " !</br>" . 
+				"Your ID number is " . $studentNo . " !</br>" . 
+				"Your Email Address is " . $emailAddress . "! </br>" .
+				"Your Password is ". $password . " ! </br>" .
+				"Your Birthday is ". $birthDate . " ! </br>" . 
+				"The Time now is " . date('c');
+			}
+			else
+			{
+				header('location: register.php');
+			}
+
+			?>
+
+            </label>
+            <br><br><br><br><br><br><br><br><br>
+          </div>
+
+          <button type="submit" class="button button-block" name="register" class="btn btn-success"/>Log Out</button>
+
+          </form>
+        </div>
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/index.js"></script>
+
+</body>
+</html>
